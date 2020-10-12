7c7276bff7ffe1314af8898d1902e5590fd41a66
Raynier van Dam <raynier@e-trailer.nl>
2020-10-12 13:57:32 +0200

    Updates

--- velocity-over-time/build/app-home.entry.js
+++ velocity-over-time/build/app-home.entry.js
@@ -1,6 +1,6 @@
 import { r as registerInstance, h } from './index-95f76a9b.js';
 
-const appHomeCss = ".app-home{padding:10px}button{background:#5851ff;color:white;margin:8px;border:none;font-size:13px;font-weight:700;text-transform:uppercase;padding:16px 20px;border-radius:2px;box-shadow:0 8px 16px rgba(0, 0, 0, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);outline:0;letter-spacing:0.04em;transition:all 0.15s ease;cursor:pointer}button:hover{box-shadow:0 3px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.1);transform:translateY(1px)}";
+const appHomeCss = ".app-home{padding:10px}.data{margin-bottom:1em}.key{font-size:24pt}.value{font-size:24pt;font-weight:bold}.flex{display:flex}.flex>div{flex-grow:1;flex-basis:0}.key,.value{text-align:center}";
 
 const AppHome = class {
   constructor(hostRef) {
@@ -44,7 +44,7 @@ const AppHome = class {
     }
   }
   render() {
-    return (h("div", { class: "app-home" }, h("div", null, "Current location: lat(", this.locations[this.locations.length - 1].coords.latitude, "), long(", this.locations[this.locations.length - 1].coords.longitude, ")"), h("div", null, "Current speed: ", (this.locations[this.locations.length - 1].coords.speed * 3.6).toFixed(2), "km/h"), h("div", { ref: el => this.graphElement = el })));
+    return (h("div", { class: "app-home" }, h("div", { class: "flex" }, h("div", { class: "data" }, h("div", { class: "key" }, "Location:"), h("div", { class: "value" }, "lat(", this.locations[this.locations.length - 1].coords.latitude.toFixed(2), "), long(", this.locations[this.locations.length - 1].coords.longitude.toFixed(2), ")")), h("div", { class: "data" }, h("div", { class: "key" }, "Speed:"), h("div", { class: "value" }, (this.locations[this.locations.length - 1].coords.speed * 3.6).toFixed(2), "km/h"))), h("div", { ref: el => this.graphElement = el })));
   }
 };
 AppHome.style = appHomeCss;
